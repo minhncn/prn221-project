dd1ce1e6bfe7d647d84c1f6e419f7f900adc57b5
Trần Thịnh <76423089+Thinhtran42@users.noreply.github.com>
2024-03-12 21:25:17 +0700

    Test Iron library for read text from image

---
+++ TestOCR/TestOCR/Class1.cs
@@ -0,0 +1,24 @@
+﻿using IronOcr;
+
+namespace TestOCR;
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        var ocr = new IronTesseract();
+
+        using (var ocrInput = new OcrInput())
+        {
+            ocrInput.AddImage("images/image.png");
+            //ocrInput.AddPdf("document.pdf");
+
+            // Optionally Apply Filters if needed:
+            // ocrInput.Deskew();  // use only if image not straight
+            // ocrInput.DeNoise(); // use only if image contains digital noise
+
+            var ocrResult = ocr.Read(ocrInput);
+            Console.WriteLine(ocrResult.Text);
+        }
+    }
+}
+
---
+++ TestOCR/TestReadImg/Program.cs
@@ -0,0 +1,29 @@
+﻿using IronOcr;
+namespace TestReadImg
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var testImagePath = @"../../../images/image3.jpeg";
+            try
+            {
+                var Ocr = new IronTesseract();
+
+                // Add a primary language (Default is English)
+                Ocr.Language = OcrLanguage.English;
+
+                // Add as many secondary languages as you like
+                Ocr.AddSecondaryLanguage(OcrLanguage.VietnameseBest);
+
+                var result = Ocr.Read(testImagePath).Text;
+
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
